fd33e77ce2719dc93d856eb38f249841acf4cede
math <mathdu@protonmail.com>
2021-11-13 06:59:12 -0300

    adding all functions to the header file

--- include/catchme.h
+++ include/catchme.h
@@ -3,10 +3,27 @@
 
 #include <stdbool.h>
 
+bool get_metadata(const char *name, char *result);
 bool get_property_bool(const char *property, bool *result);
 bool get_property_string(const char *property, char *result);
-void get_property(const char* property, char* result);
+bool get_property_double(const char *property, double *result);
+bool get_property_int(const char *property, int *result);
 void catchme_update(void);
+void catchme_remove(int id);
+void catchme_mute(void);
+void catchme_toggle(void);
+void catchme_play(void);
+void catchme_seek(char *seek);
+void catchme_idle(void);
+void catchme_playlist(void);
+void catchme_play_id(int id);
+void catchme_status(void);
+void catchme_current(void);
+void catchme_pause(void);
+void catchme_prev(void);
+void catchme_next(void);
+void catchme_volume(char *vol);
+void catchme_shuffle(void);
 
 #endif // CATCHME_H
 
